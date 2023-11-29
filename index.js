dd41a6b1d52fdc5de547438740e6aa33175d7e03
rickbroken <daironquebrada@gmail.com>
2023-11-28 21:19:17 -0500

    Version 2

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require("express");
+import express, { json } from "express";
+import { readFileSync } from "node:fs";
+
+const dittoJSON = JSON.parse(readFileSync('./ditto.json', 'utf-8'));
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Middleware para formatear la respuesta del body
-app.use(express.json());
+app.use(json());
 
 app.get('/', (req, res) => {
-  res.send('Hey bienvenido');
+  res.json(dittoJSON);
 });
 
 app.listen(PORT, () => {
